aa00ad942192c7a80e38af10ca48fad306e633c4
Joaquin Rodriguez <ertsyvler@gmail.com>
2025-09-21 16:11:40 -0300

    Version Final

--- codigo.js
+++ codigo.js
@@ -1,20 +1,4 @@
-/* codigo javascript 
-const btnCopiar = document.querySelectorAll('.btnCopiar');
-
-btnCopiar.forEach(button => {
-    button.addEventListener('click', () => {
-        const textToCopy = button.previousElementSibling.textContent;
-        navigator.clipboard.writeText(textToCopy).then(() => {
-            alert('Texto copiado al portapapeles: ' + textToCopy);
-        }
-    ).catch(err => {
-        console.error('Error al copiar el texto: ', err);
-    });
-});
-});
-
-*/
-
+/* codigo javascript */
 const copiarMail = async () => {
     const textoACopiar = 'joaquinr.erti@gmail.com'; 
 
@@ -27,12 +11,12 @@ const copiarMail = async () => {
     }
   };
 
-    const copiarTexto = async () => {
+const copiarNumero = async () => {
     const textoACopiar = '+54 11 2659-6891'; 
 
     try {
       await navigator.clipboard.writeText(textoACopiar);
-      alert('numero copiado al portapapeles: ' + textoACopiar);
+      alert('Numero copiado al portapapeles: ' + textoACopiar);
     } catch (err) {
       console.error('Error al copiar el texto: ', err);
       alert('Hubo un problema copiando el numero' );
